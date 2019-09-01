73a1be4ff3cf3ca6ee65c035d4e06a813ffd930f
douglas fletcher <fletchy_d@hotmail.com>
2019-09-02 01:17:08 +0200

    master: update default configs

--- kotlinsboot-demo-service/src/main/kotlin/com/example/rest/ExampleApplication.kt
+++ kotlinsboot-demo-service/src/main/kotlin/com/example/rest/ExampleApplication.kt
@@ -5,6 +5,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(ApplicationConfiguration::class)
 class ExampleApplication
 
 fun main(args: Array<String>) {
